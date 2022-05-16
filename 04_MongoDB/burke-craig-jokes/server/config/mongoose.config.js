b8786f01cd7d625d19f225845796fcdb4b8420d8
Craig Burke <cragardev@gmail.com>
2022-05-16 14:52:00 -0500

    working1

--- 04_MongoDB/burke-craig-jokes/server/config/mongoose.config.js
+++ 04_MongoDB/burke-craig-jokes/server/config/mongoose.config.js
@@ -1,11 +1,14 @@
 // import mongoose
 const mongoose = require("mongoose");
+const db_name = "jokes_db";
+const db_login = "cragardev";
+const db_pw = "1234567890Cb";
 
 // mongoose connection here
 mongoose
-  .connect("mongodb+srv://cragardev:1234567890Cb@dojo-mean-may.ckqcp.mongodb.net/jokes_db?retryWrites=true&w=majority", {
+  .connect(`mongodb+srv://${db_login}:${db_pw}@dojo-mean-may.ckqcp.mongodb.net/${db_name}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log("Ok Craig, Now we have established a connection to the database - jokes_db"))
-  .catch((err) => console.log("Uh-ooh, Craig, Something has gone wrong when connecting to the database, you had better check on it. look here: ", err));
+  .then(() => console.log(`Ok Craig, Now we have established a connection to the database - ${db_name}`))
+  .catch((err) => console.log(`Uh-ooh, Craig, Something has gone wrong when connecting to the database: ${db_name}, you had better check on it. look here: `, err));
